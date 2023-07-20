c317366b3dc60109c1deffdf817bac76cad4318f
Clust34 <chapuisnicolas34@gmail.com>
2023-07-20 11:28:50 +0200

    feat: add delete article

--- src/Controller/Backend/ArticleController.php
+++ src/Controller/Backend/ArticleController.php
@@ -6,6 +6,7 @@ use App\Entity\Article;
 use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -85,5 +86,32 @@ class ArticleController extends AbstractController
         return $this->render('Backend/Article/edit.html.twig', [
             'form' => $form
         ]);
+    }
+
+    #[Route('/delete', name: '.delete', methods: ['POST'])]
+    public function delete(Request $request): RedirectResponse
+    {
+        // On vas chercher le bon article de part son id si il trouve pas tu met 0 donc pas de id à 0
+        $article = $this->articleRepo->find($request->get('id', 0));
+
+        if (!$article instanceof Article) {
+            $this->addFlash('error', 'Article non trouvé');
+
+            // il n'y a aucun parametre donc tu met un tableau vide puis le code d'erreur
+            return $this->redirectToRoute('admin.articles.index', [], 404);
+        }
+
+        // On vérifie le token 
+        if ($this->isCsrfTokenValid('delete' . $article->getId(), $request->get('token'))) {
+            $this->articleRepo->remove($article);
+
+            $this->addFlash('success', 'Article supprimé avec succès');
+
+            return $this->redirectToRoute('admin.articles.index');
+        }
+
+        $this->addFlash('error', 'Token invalide');
+
+        return $this->redirectToRoute('admin.articles.index');
     }
 }
